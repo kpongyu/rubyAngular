de486f3a703f24b14226cba31e3dc63e65b73e04
kaipengyu <Kaipeng.Yu@icf.com>
2020-04-15 23:16:42 -0400

    add angular dependencies

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,7 +8,7 @@ class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
     begin
-      client = IEX::Api::Client.new(publishable_token: 'PUBLISHABLE TOKEN HERE')
+      client = IEX::Api::Client.new(publishable_token: 'pk_584cb3f9ca004c5ab443f40ea0e63562')
       looked_up_stock = client.quote(ticker_symbol)
       new(name: looked_up_stock.company_name,
           ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
